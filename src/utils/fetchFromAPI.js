5ccc10343bdd18ac8c6a705985f8715c1578c5cc
ngocpu <vanngoc51203@gmail.com>
2023-06-15 17:23:07 +0700

    first-commit

---
+++ src/utils/fetchFromAPI.js
@@ -0,0 +1,19 @@
+import axios from "axios";
+const BASEURL = 'https://youtube-v31.p.rapidapi.com'
+const options = {
+   
+    url: BASEURL,
+    params:{
+        maxResults:60
+    },
+    headers: {
+      'X-RapidAPI-Key': '3eaad778a7mshb4909cdf074b0e4p1b7bd8jsn7147fc12be14',
+      'X-RapidAPI-Host': 'youtube-v31.p.rapidapi.com'
+    }
+  };
+
+  export const fetchFromAPI = async (url) =>{
+    const {data} = await axios.get(`${BASEURL}/${url}`,options)
+
+    return data
+  }
